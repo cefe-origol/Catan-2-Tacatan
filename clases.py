c7067af2403a1673bf0003ac3521833736e883d8
Micabr <71016889+Micabr@users.noreply.github.com>
2022-09-05 14:25:00 -0300

    clases

--- clases.py
+++ clases.py
@@ -1,14 +1,16 @@
 class Asentamiento:
-    pass
+     def __init__(self,Jugador):
+         self.Jugador = Jugador
 
 class Ciudad:
     pass
 
 class Camino:
-    pass
+    def __init__(self,Jugador):
+         self.Jugador = Jugador
 
 class Jugador:
-    def __init__(self, name, colour):
-        self.nombre=name
-        self.color=colour
-        pass
+    def __init__(self,nombre,color):
+        self.nombre = nombre
+        self.color = color
+
