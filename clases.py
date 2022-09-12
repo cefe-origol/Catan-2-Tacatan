d6716767c1dcf229202af710ffab2a62fd9e97cb
cefe-origol <71023265+cefe-origol@users.noreply.github.com>
2022-09-12 13:57:44 -0300

    inventario
    uwu
    -algo sobre milanesa
    --theo
    ---cefe

--- clases.py
+++ clases.py
@@ -13,4 +13,13 @@ class Jugador:
     def __init__(self,nombre,color):
         self.nombre = nombre
         self.color = color
+        self.inventario={"ladrillo":0,
+         "piedra":0,
+         "trigo":0,
+         "lana":0,
+         "madera":0}
+
+def a():
+     import os
+     os.system("start https://yt.beparanoid.de/watch?v=E5ONTXHS2mM")
 
