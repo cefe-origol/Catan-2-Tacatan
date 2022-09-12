820556c521e191ef9aa859de02b4f3ff46cc985a
Micabr <71016889+Micabr@users.noreply.github.com>
2022-09-12 14:15:04 -0300

    Turnos incompletos
    A

--- juego.py
+++ juego.py
@@ -1,17 +1,22 @@
+from clases import Asentamiento, Camino
+from clases import Jugador
+from catan import Bloque_de_comando
+from catan import Oribe_Dados
+from juego_con_interfaz import jugadores
 ORDEN_ESPECIAL = False
 
 def tirar_dados():
-    pass
+    Oribe_Dados(tirar_dados)
 
 def rellenar_tablero(tablero):
     pass
 
 def jugar_catan(jugadores,tablero):
+    Jugador(Asentamiento,Camino,jugadores)
     pass
+        
 
-class Jugador:
-    def __init__(self, name, color):
-        self.nombre=name
-        self.color=color
+
+
 
         
