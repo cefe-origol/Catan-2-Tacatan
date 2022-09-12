f2c614364b3c67af537decfec6ae449c71d6e763
cefe-origol <71023265+cefe-origol@users.noreply.github.com>
2022-09-12 13:44:41 -0300

    uwu >w> ~rawr :3
    (theo ]
    \
    ?"\=
    )-

--- gent_tab.py
+++ gent_tab.py
@@ -1,30 +1,25 @@
-class Tablero:
-    def __init__(self):
-        self.recursos=[]
-        self.numeros=[]
-        self.generar_tablero()
-        
-    def generar_tablero(self):
-        t1=[]
-        import random
-        rec={"ladrillo":3,
-             "piedra":3,
-             "trigo":4,
-             "lana":4,
-             "madera":4}
-        for i in rec:
-            t1+=[i]*rec[i]
-        random.shuffle(t1)
-        a=list(map(lambda x:int(x,16), "2334455668899AABBC"))
-        for i in range(len(t1)):
-            t1[i]=t1[i],a[1]
-        t1.append("")
-        random.shuffle(t1)
-        a=t1.index("")
-        t1[9],t1[a]=t1[a],t1[9]
-        for i in t1:
-            self.recursos,self.numeros=i
+
+def generar_tablero(tablero):
+    t1=[]
+    import random
+    rec={"ladrillo":3,
+         "piedra":3,
+         "trigo":4,
+         "lana":4,
+         "madera":4}
+    for i in rec:
+        t1+=[i]*rec[i]
+    random.shuffle(t1)
+    a=list(map(lambda x:int(x,16), "2334455668899AABBC"))
+    for i in range(len(t1)):
+        t1[i]=t1[i],a[i]
+    t1.append("")
+    random.shuffle(t1)
+    a=t1.index("")
+    t1[9],t1[a]=t1[a],t1[9]
+    tablero=t1
 
 #NO ENTIENDO UNA GOMA ORIBE!!!
 #FIRMA -T
                 
+#generar_tablero()
